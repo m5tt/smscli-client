a6310c082744d982b32156a223a56fe2d4bd6994
m5tt <arks36@protonmail.ch>
2016-12-24 10:05:13 -0500

    inital commit, core functionality is working, as in can receive and send messages, lots of fixing up to do

---
+++ smscli-client.py
@@ -0,0 +1,442 @@
+#/usr/bin/python
+
+import sys
+import json
+import struct
+import signal
+import socket
+import datetime
+import threading
+import collections
+
+import urwid
+
+COMMAND_PREFIX = '/'        ## all commands start with /
+VIEW_SWITCH_KEY = 'meta'    ## meta/alt key used as prefix for swtiching views
+MAX_MESSAGE_LENGTH = 160    ## each message length max 160 characters
+
+## Main data structure, all contacts and conversations with each are stored here
+contact_views = {}
+
+class ViewMessage(urwid.Padding):
+    """ Represents a single message in a view """ 
+
+    TIMESTAMP_ATTR = 'timestamp'
+    SENDER_ATTR = 'sender'
+    BODY_ATTR = 'body'
+
+    def __init__(self, timestamp, related_view_id, body):
+        ## TODO: format timestamp and body in a generic manner
+        self.timestamp = timestamp
+        self.related_view_id = related_view_id
+        self.body = body
+
+        self.alignment = 'left'
+        self.width_type = 'relative'
+        self.width_size = 60
+
+        super().__init__(urwid.Text([
+                (ViewMessage.TIMESTAMP_ATTR, self.timestamp),
+                (ViewMessage.SENDER_ATTR, self.related_view_id),
+                (ViewMessage.BODY_ATTR, self.body)
+            ]),
+            align=self.alignment,
+            width=(self.width_type, self.width_size)
+        )
+
+
+class View(object):
+    """
+        Represents a single View.
+
+        Encapsulates both the ListBox and the ListWalker,
+        so we can easily switch views and add to them
+
+        content is a list of ViewMessages
+    """
+
+    def __init__(self, view_id, view_name, content):
+        self.view_id = view_id
+        self.view_name = view_name
+        self.listwalker = urwid.SimpleFocusListWalker(content)
+        self.listbox = urwid.ListBox(self.listwalker)
+
+    def add_message(self, view_message):
+        self.listwalker.append(view_message)
+
+    def scroll_to_bottom(self):
+        self.listbox.set_focus(len(self.listwalker) - 1)
+
+    def add_message(self, view_message):
+        self.listwalker.append(view_message)
+        self.scroll_to_bottom()
+
+
+class LogView(View):
+    """
+        The main central view, that smscli logs output too
+    """
+
+    VIEW_ID = '0'
+    VIEW_NAME = 'smscli'
+
+    def __init__(self, content):
+        super().__init__(self.VIEW_ID, self.VIEW_NAME, content)
+
+    def print_message(self, message):
+        self.add_message(ViewMessage(
+            datetime.datetime.now().time().strftime('%H:%M:%S'),
+            LogView.VIEW_NAME,
+            message
+        ))
+
+
+class ContactView(View):
+    """
+        The view for the conversation with a
+        contact
+
+        Holds contact data along with urwid widgets
+        to represent it as a view
+    """
+
+    def __init__(self, view_id, display_name, address, content):
+        self.display_name = display_name
+        self.address = address
+        
+        super().__init__(view_id, display_name, content)
+
+
+class MainWindow(urwid.Frame):
+    """
+        Represents the main window that holds
+        the different views
+
+        Manages setting up whole interface,
+        adding new views, switching between them...
+
+        key events will be handled externally
+    """
+
+    TITLE_BAR_ATTR = 'titlebar'
+    TITLE_BAR_TEXT = 'smscli'
+
+    DIVIDER_ATTR = 'divider'
+    DIVIDER_INIT_TEXT = '-0-'
+
+    EDIT_CAPTION = '> '
+    MAX_VIEWS = 6
+
+    def __init__(self, init_view):
+        self.shown_views = collections.OrderedDict({init_view.view_id: init_view })
+        self.view_count = 1;
+        self.max_views = 6
+        self.current_view = init_view
+
+        self.title_bar = urwid.AttrMap(urwid.Text(MainWindow.TITLE_BAR_TEXT), MainWindow.TITLE_BAR_ATTR)
+        self.divider_text = self.DIVIDER_INIT_TEXT
+        self.divider = urwid.AttrMap(urwid.Text(self.divider_text), MainWindow.DIVIDER_ATTR)
+        self.input_line = urwid.Edit(MainWindow.EDIT_CAPTION)
+
+        inner_frame = urwid.Frame(
+                list(self.shown_views.items())[0][1].listbox,
+                header=self.title_bar,
+                footer=self.divider
+        )
+
+        super().__init__(inner_frame, footer=self.input_line)
+
+        self.set_focus('footer')
+
+    def add_new_view(self, view):
+        if (self.view_count <= self.max_views):
+            self.shown_views[view.view_id] = view
+
+            ## TODO: Fix this whole shit
+            self.divider_text += '[' + str(self.view_count) + ']'
+            self.divider.original_widget.set_text(self.divider_text)
+            self.view_count += 1
+
+    def switch_view(self, view_id):
+        """
+            change views, as in switch the current listbox
+            in the frame with a different one
+            contents['body'][0] -> inner frame
+            contents['body'][0].contents['body'] -> (listbox, attr)
+        """
+
+        self.contents['body'][0].contents['body'] = (self.shown_views[view_id].listbox, None)
+        self.current_view = self.shown_views[view_id]
+
+    def get_input(self):
+        return self.input_line.get_edit_text()
+
+    def clear_input(self):
+        self.input_line.set_edit_text('')
+
+
+class ConnectionHandler(object):
+    '''
+        Handles all connection with the server
+
+        protocol with server:
+            communicate in messages
+            messages are JSON strings
+
+            messages can be sent either to or from
+            server/client at any time
+
+            initial data: large contact list with sms conversations
+                          encapsulated
+
+            messages after: sms messages belonging to a 
+                            conversation
+
+            on connection: client reads initial data
+            write: send message length in bytes - size 4 bytes
+                   send data of size s
+            read:  read 4 bytes to get length len
+                   read len bytes
+    '''
+
+    LEN_SIZE = 4
+    LEN_STRUCT_FORMAT = '! i'
+
+    def __init__(self):
+        self.connected = False
+
+    def setup_connection(self, ip_address, port):
+        ''' 
+            connects to server, reads initial data
+            starts up read loop thread
+        '''
+
+        self.connect(ip_address, port)
+        initial_data = self.read_server()
+        self.read_looper = threading.Thread(target=self.read_loop)
+        self.read_looper.start()
+
+        JSONHelper.setup_contact_view_list(initial_data)
+
+    def connect(self, ip_address, port):
+        try:
+            self.ip_address = socket.inet_aton(ip_address)
+            self.port = port
+
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((ip_address, int(port)))
+            self.connected = True
+            log_view.print_message('Connected to ' + str(self.ip_address) + ' on port: ' + str(self.port))
+        except socket.error as e:
+            log_view.print_message(str(e))
+
+    def read_server(self):
+        ''' reads a json string from the server '''
+        
+        message = ''
+
+        try:
+            length = int.from_bytes(
+                    self.socket.recv(self.LEN_SIZE, socket.MSG_WAITALL),
+                    'big'
+            )
+            message = str(self.socket.recv(length, socket.MSG_WAITALL), 'utf-8')
+        except socket.error as e:
+            log_view.print_message(str(e))
+
+        return message
+
+    def write_server(self, message):
+        '''
+            write a json string to the server
+
+            we use the struct module to correctly send
+            the integer size, using network byte order/endianess
+        ''' 
+
+        try: 
+            self.socket.sendall(struct.pack(ConnectionHandler.LEN_STRUCT_FORMAT, len(message)))
+            self.socket.sendall(message.encode('utf-8'))
+        except socket.error as e:
+            log_view.print_message(str(e))
+
+    def read_loop(self):
+        '''
+            indefinitely blocks and reads server
+            on read, updates data structure
+        '''
+
+        while 1:
+            json_message = self.read_server()
+
+            view_message = JSONHelper.json_to_view_message(json_message)
+            related_contact_view = contact_views[view_message.related_view_id]
+            related_contact_view.add_message(view_message)
+
+            if view_message.related_view_id not in main_window.shown_views:
+                main_window.add_new_view(related_contact_view)
+
+            main_loop.draw_screen()
+
+            ## TODO: Raise notification here
+            
+
+class CommandHandler(object):
+    """
+        Parses and handles all commands
+
+        Handles some, delegates others to
+        external objects 
+    """
+
+    def parse_command(self, command):
+        command_parts = command[1:].split()
+        command_name = command_parts[0]
+        command_args = command_parts[1:]
+
+        ## TODO: handle args properly
+
+        if (command_name == 'connect'):
+            connection_handler.setup_connection(
+                    command_args[0], 
+                    command_args[1]
+            )
+
+    def print_help(self, command):
+        pass
+
+
+class JSONHelper(object):
+    """
+        Util methods for converting between 
+        JSON strings and objects used here
+
+        Is aware of dict keys used in json
+    """
+
+    def view_message_to_json(view_message):
+        view_message_dict = {
+                'timestamp': view_message.timestamp,
+                'body': view_message.body,
+                'relatedContactId': view_message.related_view_id,
+                'smsMessageType': 'OUTGOING'
+        }
+
+        return json.dumps(view_message_dict)
+
+    def json_to_view_message(json_view_message):
+        view_message_dict = json.loads(json_view_message)
+        return ViewMessage(
+                '17:54:33', ## TODO: parse timestamp
+                view_message_dict['relatedContactId'],
+                view_message_dict['body']
+        )
+
+    def dict_to_contact_view(contact_view_dict):
+        """ Convert a contact view dict to a ContactView object """
+
+        ## Convert list of sms dictionaries to a list of ViewMessage objects
+        view_messages = []
+        for view_message_dict in contact_view_dict['conversation']:
+            view_messages.append(ViewMessage(
+                '17:54:33', ## TODO: parse timestamp
+                view_message_dict['relatedContactId'],
+                view_message_dict['body']
+            ))
+
+        ## we want most recent message to be last
+        view_messages = list(reversed(view_messages))
+
+        return ContactView(
+                contact_view_dict['id'],
+                contact_view_dict['displayName'],
+                contact_view_dict['phoneNumber'],
+                view_messages
+        )
+                
+    def setup_contact_view_list(json_contact_list):
+        """ Convert json to a contact view list """
+
+        contact_view_dicts = json.loads(json_contact_list)
+
+        for contact_view_dict in contact_view_dicts:
+            contact_view = JSONHelper.dict_to_contact_view(contact_view_dict)
+            contact_views[contact_view.view_id] = contact_view
+
+        for key in contact_views.keys():
+            log_view.print_message(key)
+
+
+def handle_view_switch(key):
+    try:
+        view_index = int(key.split()[1])
+
+        if (view_index < len(main_window.shown_views)):
+            main_window.switch_view(
+                    list(main_window.shown_views.items())[view_index][0]
+            )
+    except:
+        pass
+
+
+def handle_send_message(message):
+    """ create a ViewMessage given 
+        message  body and current view
+        then send and add to view
+    """
+
+    if len(message) <= MAX_MESSAGE_LENGTH:
+        view_message = ViewMessage(
+                datetime.datetime.now().time().strftime('%H:%M:%S'),
+                main_window.current_view.view_id,
+                message
+        )
+
+        connection_handler.write_server(JSONHelper.view_message_to_json(view_message))
+        main_window.current_view.add_message(view_message)
+
+        main_window.clear_input()
+    else:
+        log_view.print_message('Message to long')
+
+def ctrlc_quit(signum, frame):
+    """ trap ctrl-c """
+
+    raise urwid.ExitMainLoop
+
+
+def handle_input(key):
+    if key == 'enter':
+        user_input = main_window.get_input()
+        if user_input[0] == COMMAND_PREFIX:
+            command_handler.parse_command(user_input)
+            main_window.clear_input()
+        elif (connection_handler.connected) and (main_window.current_view != log_view):
+            handle_send_message(user_input)
+    elif VIEW_SWITCH_KEY in key:
+        handle_view_switch(key)
+
+
+
+## TODO: get this from a file
+palette = [
+    ('timestamp', 'dark blue', 'default'),
+    ('sender', 'dark blue', 'default'),
+    ('titlebar', 'black', 'dark blue'),
+    ('divider', 'black', 'dark blue')
+]
+
+
+if __name__ == '__main__':
+    command_handler = CommandHandler()
+    connection_handler = ConnectionHandler()
+
+    log_view = LogView([])
+    main_window = MainWindow(log_view)
+
+    signal.signal(signal.SIGINT, ctrlc_quit)
+
+    log_view.print_message('Welcome to smscli')
+
+    main_loop = urwid.MainLoop(main_window, palette, unhandled_input=handle_input)
+    main_loop.run()
